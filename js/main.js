5976f70a6bd43632173e53a6ff30f011e9b8d4af
Lovaro <lorenavaleriarodriguez@gmail.com>
2020-10-27 05:19:35 -0300

    funcionalidad con jquery

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(".toggle").click(function () {
+  $(".formulario").animate(
+    {
+      height: "toggle",
+      "padding-top": "toggle",
+      "padding-bottom": "toggle",
+      opacity: "toggle",
+    },
+    "slow"
+  );
+});
